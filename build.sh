5810fe779a25ddd98fde14bc2bfa251c1aeaa344
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2025-11-21 00:53:52 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ unzip ../libs1.zip
 unzip ../libs2.zip
 cd ./..
 pip install audioop
-wget https://huggingface.co/yustudiojp/gguf-models/resolve/main/mania-model.tq2_0_K_M.gguf
-python discord_bot.py mania-model.tq2_0_K_M.gguf "あなたはウェブマニアです。適切に答えて下さい。"
+wget https://huggingface.co/yustudiojp/gguf-models/resolve/main/mania-model.Q8_K_M.gguf
+python discord_bot.py mania-model.Q8_0_K_M.gguf "あなたはウェブマニアです。適切に答えて下さい。"
